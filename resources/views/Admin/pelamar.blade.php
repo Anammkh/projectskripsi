ffffed36a8ea5b2f890edf0f25194914ce3c3a15
haaeell <haikaljawahir434@gmail.com>
2024-06-24 13:04:08 +0700

    fix: pelamar detail

--- resources/views/Admin/pelamar.blade.php
+++ resources/views/Admin/pelamar.blade.php
@@ -11,45 +11,62 @@
                     </div>
                 @endif
                 <h4 class="header-title mt-0 mb-1">Daftar pelamar</h4>
-                <table id="basic-datatable" class="table dt-responsive nowrap w-100">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama</th>
-                            <th>alamat</th>
-                            <th>TTL</th>
-                            <th>Status</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($pelamars as $pelamar)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $pelamar->user->name}}</td>
-                            <td>{{ $pelamar->alamat }}</td>
-                            <td>{{ $pelamar->ttl }}</td>
-                            <td>{{ $pelamar->status }}</td>
-                            <td>
-                                @if ($pelamar->status == 'Menunggu')
-                                <div class="d-flex">
-                                    <form action="{{ route('pelamars.accept', $pelamar->id) }}" method="POST" style="display: inline;">
-                                        @csrf
-                                        <button type="submit" class="btn btn-success mr-2">Terima</button>
-                                    </form>
-                                    <form action="{{ route('pelamars.reject', $pelamar->id) }}" method="POST" style="display: inline;">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger">Tolak</button>
-                                    </form>
-                                </div>
-                                @else
-                                    <span class="badge {{ $pelamar->status == 'Diterima' ? 'badge-success' : 'badge-danger' }}">{{ $pelamar->status }}</span>
-                                @endif
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table id="basic-datatable" class="table dt-responsive nowrap w-100">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Jenis Kelamin</th>
+                                <th>TTL</th>
+                                <th>Sekolah</th>
+                                <th>Alamat</th>
+                                <th>Tinggi</th>
+                                <th>No HP</th>
+                                <th>Jurusan</th>
+                                <th>Skill</th>
+                                <th>CV</th>
+                                <th>KTP</th>
+                                <th>Transkip Nilai</th>
+                                <th>Ijazah</th>
+                                <th>Kota</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pelamars as $pelamar)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $pelamar->user->name }}</td>
+                                <td>{{ $pelamar->jenis_kelamin }}</td>
+                                <td>{{ $pelamar->ttl }}</td>
+                                <td>{{ $pelamar->sekolah }}</td>
+                                <td>{{ $pelamar->alamat }}</td>
+                                <td>{{ $pelamar->tinggi }}</td>
+                                <td>{{ $pelamar->no_hp }}</td>
+                                <td>{{ $pelamar->jurusan->nama }}</td>
+                                <td>
+                                    @foreach ($pelamar->skils as $skil)
+                                        <span>{{ $skil->nama }},</span>
+                                    @endforeach
+                                </td>
+                                <td>@if ($pelamar->cv)
+                                    <a href="{{ asset($pelamar->cv) }}" target="_blank">Lihat CV</a>
+                                @endif</td>
+                                <td>@if ($pelamar->ktp)
+                                    <a href="{{ asset($pelamar->ktp) }}" target="_blank">Lihat KTP</a>
+                                @endif</td>
+                                <td>@if ($pelamar->transkip_nilai)
+                                    <a href="{{ asset($pelamar->transkip_nilai) }}" target="_blank">Lihat Transkip Nilai</a>
+                                @endif</td>
+                                <td>@if ($pelamar->ijazah)
+                                    <a href="{{ asset($pelamar->ijazah) }}" target="_blank">Lihat Ijazah</a>
+                                @endif</td>
+                                <td>{{ $pelamar->kota }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div> <!-- end card body-->
         </div> <!-- end card -->
     </div><!-- end col-->
